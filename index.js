25abdefdb1586cea520fef4de396e809c7e657d6
yi <mavisluan83@gmail.com>
2020-01-08 09:45:47 -0800

    run javascript in node

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 /* eslint-disable no-console */
 console.log('Testing');
+// Module Wrapper Function
+// (function (exports, require, module, __filename, __dirname) {
+
+// })
+/*
+    - __dirname --> abolute path to current file
+    - __filename --> absolute path + fileName
+ */
+console.log('dirname', __dirname); // absolute path
+console.log('filename', __filename); // absolute path + fileName
+
+const Person = require('./person');
+// load local module
+const person1 = new Person('John Doe', 30);
+person1.greeting();
---
+++ person.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greeting() {
+        console.log(`My name is ${this.name} and I am ${this.age}.`);
+    }
+}
+
+module.exports = Person;
