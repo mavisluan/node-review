5bf3dc168569aef5611c5366754449e0c5f9d90c
yi <mavisluan83@gmail.com>
2020-01-08 10:40:38 -0800

    Added http demo

---
+++ reference/http_demo.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+const http = require('http');
+
+// Create server object
+http.createServer((req, res) => {
+    // Write response
+    res.write('Hello World');
+    res.end();
+}).listen(5000, () => console.log('Server running ...'));
