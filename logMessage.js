d39997f761695f7066e217d0314094006ac84b26
yi <mavisluan83@gmail.com>
2020-01-08 10:49:14 -0800

    extracted logMessage and plainjs from index.js

---
+++ logMessage.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+const Logger = require('./logger');
+
+const logger = new Logger();
+logger.on('message', data => console.log('Called Listener', data));
+
+logger.log('Hello World');
+logger.log('Hi');
+logger.log('Hello');
